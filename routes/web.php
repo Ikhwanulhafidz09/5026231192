04952ec2b9ffd8ec2bded9aa637aad838d81bce7
Ikhwanulhafidz09 <ikhwanulhafidz1@gmail.com>
2025-05-21 02:09:08 +0700

    tugas pertemuan 11

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::view('/pertemuan1', 'pertemuan1');
+Route::view('/pertemuan3', 'pertemuan3');
+Route::view('/clonelinktree', 'clonelinktree');
+Route::view('/pertemuan7-js1', 'pertemuan7-js1');
+Route::view('/pertemuan7-js2', 'pertemuan7-js2');
+Route::view('/ets', 'ets');
